6e50fd11899993452f676af510ad3909ca87209d
tawanjs008-lab <tawanjs008@gmail.com>
2026-09-11 14:46:48 +0700

    มีการแก้ forgot

--- lib/views/forgot_ui.dart
+++ lib/views/forgot_ui.dart
@@ -10,6 +10,8 @@ class ForgotUI extends StatefulWidget {
 class _ForgotUIState extends State<ForgotUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
   }
 }
